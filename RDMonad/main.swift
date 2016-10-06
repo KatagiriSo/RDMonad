376c9c4d7807f56660bf0411e0d2efa8f59fe10c
katagiri <so@katagiri>
2016-10-06 15:51:31 +0900

    add Environment Monad

--- RDMonad/main.swift
+++ RDMonad/main.swift
@@ -77,5 +77,90 @@ func getAllRecords() -> Process<[SomeRecord]> {
     print(records)
 }
 
+// Environment Monad
+
+typealias Environment<E,T> = (E)->T
+
+func unit<E,T>(_ t:T) -> Environment<E,T> {
+    
+    let environment = {(e:E) -> T in
+        return t
+    }
+    
+    return environment
+}
+
+func >>=<E,T,U>(left:@escaping Environment<E,T>, right:@escaping (T)->Environment<E,U>) -> Environment<E,U> {
+    let environment = {(e:E) -> U in
+        
+        let t = left(e)
+        let env = right(t)
+        let u = env(e)
+        return u
+    }
+    
+    return environment
+}
+
+let count:Int = 0
+
+func hello(x:String)->Environment<Int,String> {
+    let env = {(e:Int) -> String in
+        
+        switch e {
+        case 0:
+            return "hello, \(x)."
+        case 1:
+            return "hi."
+        default:
+            return "..."
+        }
+    }
+    
+    return env
+}
+
+func math(input:String)->Environment<Int,Int> {
+    let env = {(e:Int) -> Int in
+        switch e {
+        case 0:
+            return input.lengthOfBytes(using: .utf8)
+        case 1:
+            return input.lengthOfBytes(using: .utf8) * 5
+        default:
+            return 0
+        }
+    }
+    
+    return env
+}
+
+
+let helloEnv = hello(x: "poi")
+let mathEnv = math(input: "hoge")
+
+let result = hello(x: "poi") >>= math
+
+print("helloEnv(0) == \(helloEnv(0))")
+print("helloEnv(1) == \(helloEnv(1))")
+print("helloEnv(2) == \(helloEnv(2))")
+
+print("mathEnv(0) == \(mathEnv(0))")
+print("mathEnv(1) == \(mathEnv(1))")
+print("mathEnv(2) == \(mathEnv(2))")
+
+print("result(0) == \(result(0))")
+print("result(1) == \(result(1))")
+print("result(2) == \(result(2))")
+
+
+
+
+
+
+
+
+
+
 
 
